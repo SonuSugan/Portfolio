aec785304794b3c76794e5045a7a1b0b92a4b649
SonuSugan <www.sonu1920@gmail.com>
2024-06-03 01:06:51 +0530

    conn

--- server/index.js
+++ server/index.js
@@ -1,12 +1,9 @@
 require("dotenv").config();
-
 const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
 const contactRouter = require("./router/auth-router");
 const app = express();
-
-
 
 const corsOptions = {
   origin: "https://portfolio-gold-pi-77.vercel.app",
@@ -16,27 +13,38 @@ const corsOptions = {
   optionsSuccessStatus: 200, // For legacy browser support
 };
 
+// Middleware to handle CORS
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
-
-const PORT = process.env.PORT || 5000;
+app.options("*", cors(corsOptions)); // Include preflight response for all routes
 
 // Middleware to parse JSON
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  // useNewUrlParser: true,
-  // useUnifiedTopology: true,
-}).then(() => {
-  console.log("Connected to MongoDB Atlas");
-}).catch((error) => {
-  console.error("Error connecting to MongoDB Atlas:", error);
-});
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB Atlas:", error);
+  });
 
 // Use the contact router
 app.use("/api/contact", contactRouter);
 
+app.use((req, res, next) => {
+  res.on("finish", () => {
+    console.log("Response Headers:", res.getHeaders());
+  });
+  next();
+});
+
+// Listen to the defined port
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const ContactForm = require("../controllers/auth-controllers");
 router.route("/").post(ContactForm);
 
-
+// router.options("/", cors()); // Handle preflight request for this route
+router.post("/", ContactForm);
 
 module.exports = router;

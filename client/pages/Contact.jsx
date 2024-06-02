23d04435c74f1d2c3410ae9cdf55ad213c4534fd
SonuSugan <www.sonu1920@gmail.com>
2024-06-03 00:41:15 +0530

    conn

--- client/pages/Contact.jsx
+++ client/pages/Contact.jsx
@@ -25,7 +25,7 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch(`http://localhost:5000/api/contact`, {
+      const response = await fetch(`portfolio-api-lyart.vercel.app`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",

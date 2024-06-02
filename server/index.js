43f91bddce721962c5da11c15525f81e36b6c3f1
SonuSugan <www.sonu1920@gmail.com>
2024-06-02 23:56:11 +0530

    con

--- server/index.js
+++ server/index.js
@@ -23,8 +23,8 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
 }).then(() => {
   console.log("Connected to MongoDB Atlas");
 }).catch((error) => {
--- server/models/auth_model.js
+++ server/models/auth_model.js
@@ -8,3 +8,4 @@ const contactSchema = new Schema({
 
 const Contact = new model("Contact", contactSchema);
 module.exports = Contact;
+ 

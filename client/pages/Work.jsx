5d1657c241a8297ba9c49100db98992aef278ec5
SonuSugan <www.sonu1920@gmail.com>
2024-06-06 10:30:32 +0530

    Test 1

--- client/pages/Work.jsx
+++ client/pages/Work.jsx
@@ -32,6 +32,28 @@ const Work = () => {
             </div>
           </div>
         </div>
+        <div className="Work-center">
+          <div className="Work-detail">
+            <span className="span4">NEXT Education</span>
+            <span className="span5">
+              During my internship at Next Education, Developed and implemented
+              report cards and certificates for the school using HTML and CSS.{" "}
+            </span>
+            <span className="span5">
+              Uploading and managing student, staff, and school data provided by
+              the institution in the Learning Management System (LMS).
+            </span>
+            <span className="span5">
+              Maintaining the LMS, ensuring the integrity and accuracy of school
+              data, and providing ongoing support and updates.
+            </span>
+          </div>
+          <div className="Work-intro">
+            <div className="container">
+              <img src="/Next.png" alt="" />
+            </div>
+          </div>
+        </div>
       </div>
     </Wrapper>
   );
--- client/utils/Link.jsx
+++ client/utils/Link.jsx
@@ -1,8 +1,5 @@
 import { FaHome } from "react-icons/fa";
-import { GiSkills } from "react-icons/gi";
-import { GiAerialSignal } from "react-icons/gi";
 import { GrContactInfo } from "react-icons/gr";
-import { FcAbout } from "react-icons/fc";
 import { BsPersonLinesFill } from "react-icons/bs";
 import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 import { MdOutlineWorkOutline } from "react-icons/md";
